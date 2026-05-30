668879794b1774f9244387958e2e12054152b6ec
Sairaj Dhamal <sweeppvt06@gmail.com>
2026-05-30 18:36:04 +0530

    Add: interleaving-string - submission-0

---
+++ Data Structures & Algorithms/interleaving-string/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    Boolean[][] dp ;
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        int n = s1.length();
+        int m = s2.length();
+
+        if((n+m) != s3.length()) return false;
+
+        dp = new Boolean[m+1][n+1];
+
+        return dfs(0,0,0,s1,s2,s3);
+        
+    }
+
+    private boolean dfs(int i , int j , int k ,String s1, String s2, String s3 ){
+
+        if(k == s3.length()){
+            return (i == s1.length()) && (j ==s2.length());
+        }
+
+        if(dp[i][j] != null) return dp[i][j];
+
+       boolean result = false ;
+        if(i<s1.length() && s1.charAt(i)==s3.charAt(k)){
+           result=dfs(i+1,j,k+1,s1,s2,s3);
+        }
+
+        if(!result && j<s2.length() && s2.charAt(j)==s3.charAt(k)){
+           result=dfs(i,j+1,k+1,s1,s2,s3);
+        }
+
+        dp[i][j]=result;
+
+        return result ;
+    }
+}
